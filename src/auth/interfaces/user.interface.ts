7e046131ae2eb68e88d4d72c9c487ca41e5800de
Zaiidmo <Zaiidmoumnii@gmail.com>
2025-01-10 09:40:50 +0100

    AUTH: Create #rolesDecorator #rolesGuard #rolesEnum and update IUser Interface

--- src/auth/interfaces/user.interface.ts
+++ src/auth/interfaces/user.interface.ts
@@ -1,9 +1,11 @@
-export interface User {
+import { UserRole } from "../enums/role.enum";
+
+export interface IUser {
   uid: string;
   email: string;
   password: string;
   username: string;
-  role: string;
+  role: UserRole;
   avatar: string;
   createdAt: Date;
   updatedAt: Date;
