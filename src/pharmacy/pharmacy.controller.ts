76d8e479f707188d579b434d4f0a3ba01bc3498a
OSMaben <freegiftcardishere@gmail.com>
2025-01-20 09:49:15 +0100

    completing the crud of the pharmacy

--- src/pharmacy/pharmacy.controller.ts
+++ src/pharmacy/pharmacy.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { PharmacyService } from './pharmacy.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { CreatePharmacyDto } from './dto/create-pharmacy.dto';
 import { UpdatePharmacyDto } from './dto/update-pharmacy.dto';
-
+import {S3Service} from  '../s3/s3.service';
 @Controller('pharmacy')
 export class PharmacyController {
-  constructor(private readonly pharmacyService: PharmacyService) {}
+  constructor(
+    private readonly pharmacyService: PharmacyService,
+    private readonly s3Service: S3Service
+  ) {}
 
-  @Post()
-  create(@Body() createPharmacyDto: CreatePharmacyDto) {
+  @Post("/createPharmacy")
+  @UseInterceptors(FileInterceptor('image'))
+  async create(
+    @Body() createPharmacyDto: CreatePharmacyDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    if (file) {
+      const folder = 'pharmacies'; 
+      const uploadedImageUrl = await this.s3Service.uploadFile(file, folder);
+      console.log(uploadedImageUrl);
+      createPharmacyDto.image = uploadedImageUrl; 
+    }
+
+   
     return this.pharmacyService.create(createPharmacyDto);
   }
 
-  @Get()
+  @Get()  
   findAll() {
     return this.pharmacyService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.pharmacyService.findOne(+id);
+    return this.pharmacyService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePharmacyDto: UpdatePharmacyDto) {
-    return this.pharmacyService.update(+id, updatePharmacyDto);
+    return this.pharmacyService.update(id, updatePharmacyDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.pharmacyService.remove(+id);
+    return this.pharmacyService.remove(id);
   }
 }
